71f32cbc04f7ae61fb6cf7601da68241c8fb4fb1
Jonah Ringdahl <JonahRingdahl@outlook.com>
2026-05-08 13:35:18 -0400

    Start impl memory

--- Chip8App/Models/Memory.cs
+++ Chip8App/Models/Memory.cs
@@ -2,7 +2,12 @@ using System;
 
 namespace Chip8.Models;
 
-public class Memory
+public class Memory()
 {
+    private const uint MEMORY_END = 0xFFF;
+    private const uint MEMORY_START = 0x0;
+    private const uint MEMORY_PROGRAM_START = 0x200;
+    private const uint MEMORY_ETI_660_PROGRAM_START = 0x600;
+    private byte[] data = new byte[MEMORY_END];
 
 }
