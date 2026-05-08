831f5cd5af017c87b66b9aa6478d5c963c77ac4b
Jonah Ringdahl <JonahRingdahl@outlook.com>
2026-05-08 10:22:52 -0400

    mv readme

--- Chip8App/Models/Display.cs
+++ Chip8App/Models/Display.cs
@@ -6,9 +6,12 @@ namespace CHIP8.Models;
 class Display
 {
     Vector2 SCREEN_DIMENSIONS = new(1280, 720);
-    Vector2 DISPLAY_DIMENSIONS = new(64, 32);
+    public const int DISPLAY_WIDTH = 64;
+    public const int DISPLAY_HEIGHT = 32;
     Texture2D texture2D = new();
     private readonly float scale = 1;
+    public Color[] PixelBuffer { get; private set; } = 
+        new Color[DISPLAY_WIDTH*DISPLAY_HEIGHT];
 
     public Display(int width, int height, string title, float scale)
     {
@@ -21,17 +24,17 @@ class Display
         this.scale = scale;
     }
 
-    public void Draw(ref Color[] pixel_buffer)
+    public void Draw()
     {
         Raylib.BeginDrawing();
         Raylib.ClearBackground(Color.RayWhite);
 
-        for (int i = 0; i < pixel_buffer.Length; i++)
+        for (int i = 0; i < PixelBuffer.Length; i++)
         {
             var x = i % 64;
             var y = i / 64;
 
-            pixel_buffer[i] = (i % 3) switch
+            PixelBuffer[i] = (i % 3) switch
             {
                 0 => Color.Green,
                 1 => Color.Red,
@@ -49,7 +52,7 @@ class Display
         var x_offset = (screenWidth - chipWidth) / 2;
         var y_offset = (screenHeight - chipHeight) / 2;
 
-        UpdateTextureUnsafe(texture2D, pixel_buffer);
+        UpdateTextureUnsafe(texture2D, PixelBuffer);
 
         Raylib.DrawTextureEx(texture2D, 
         new Vector2(x_offset,y_offset),
