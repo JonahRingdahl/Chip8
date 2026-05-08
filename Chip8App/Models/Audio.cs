dbebd60cd960aefb89b950364611d1fedb0e2cb6
Jonah Ringdahl <JonahRingdahl@outlook.com>
2026-05-08 13:34:34 -0400

    Start Audio

---
+++ Chip8App/Models/Audio.cs
@@ -0,0 +1,24 @@
+using System;
+using Raylib_cs;
+
+namespace Chip8App.Models;
+
+public class Audio
+{
+    AudioStream audioStream = new();
+    public Audio()
+    {
+        Raylib.InitAudioDevice();
+        
+    }
+
+    public void Play()
+    {
+    }
+
+    ~Audio()
+    {
+        Raylib.CloseAudioDevice();
+    }
+
+}
