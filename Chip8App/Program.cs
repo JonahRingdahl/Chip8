bf06039440428743ad96fe046a4dfd32202511b5
Jonah Ringdahl <JonahRingdahl@outlook.com>
2026-05-08 13:33:47 -0400

    Reference

--- Chip8App/Program.cs
+++ Chip8App/Program.cs
@@ -1,7 +1,15 @@
-﻿using CHIP8.Models;
+﻿
+///
+/// Data Source :
+///     Cowgod's Chip-8 Technical Reference v1.0
+///     http://devernay.free.fr/hacks/chip8/C8TECH10.HTM
+/// 
+/// 
+
+using CHIP8.Models;
 
 const int WINDOW_WIDTH = 1280;
 const int WINDOW_HEIGHT = 720;
 
-var engine = new Engine(WINDOW_WIDTH, WINDOW_HEIGHT, "CHIP-8", 10);
+var engine = new CHIP8.Models.Chip8(WINDOW_WIDTH, WINDOW_HEIGHT, "CHIP-8", 10);
 engine.Run();
