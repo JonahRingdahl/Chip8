dd270537bef1890862e3f37988c71bdc2ebef9e2
Jonah Ringdahl <JonahRingdahl@outlook.com>
2026-05-13 11:59:41 -0400

    start buffer impl

--- Chip8App/Models/CPU.cs
+++ Chip8App/Models/CPU.cs
@@ -1,3 +1,5 @@
+using Raylib_cs;
+
 namespace Chip8App.Models;
 // http://devernay.free.fr/hacks/chip8/chip8def.htm
 class CPU()
@@ -8,6 +10,10 @@ class CPU()
   UInt16 pc = 0;
   UInt16 sp = 0;
 
+  public bool[] DisplayBuffer { get; } =
+    new Color[DISPLAY_WIDTH * DISPLAY_HEIGHT];
+
+
   public void Tick()
   {
 
@@ -17,37 +23,41 @@ class CPU()
   {
     switch (opcode & 0xF000)
     {
-      case 0x0000: switch (opcode & 0x00FF)
+      case 0x0000:
+        switch (opcode & 0x00FF)
         {
-          case 0x0000: { SYS_ADR_0NNN(cpu, (ushort) (opcode & 0x0FFF)); } break;
+          case 0x0000: { SYS_ADR_0NNN(cpu, (ushort)(opcode & 0x0FFF)); } break;
           case 0x00E0: { CLS_00E0(cpu, opcode); } break;
           case 0x00EE: { RET_00EE(cpu); } break;
-        } break;
+        }
+        break;
 
-      case 0x1000: { JP_ADR_1NNN(cpu, (ushort) (opcode & 0x0FFF)); } break;
-      case 0x2000: { CALL_ADR_2NNN(cpu, (ushort) (opcode & 0x0FFF)); } break;
-      case 0x3000: { SE_Vx_3XKK(cpu, (ushort) ((opcode & 0x0F00) >> 2), (ushort) (opcode & 0x00FF)); } break;
-      case 0x4000: { SNE_Vx_4XKK(cpu, (ushort) ((opcode & 0x0F00) >> 2), (ushort) (opcode & 0x00FF)); } break;
-      case 0x5000: { SE_VxVy_5XY0(cpu, (ushort) ((opcode & 0x0F00) >> 2), (ushort) ((opcode & 0x00F0) >> 1)); } break;
-      case 0x6000: { LD_Vx_6XKK(cpu, (ushort) ((opcode & 0x0F00) >> 2), (ushort) ((opcode & 0x00FF) >> 1)); } break;
-      case 0x7000: { ADD_Vx_7XKK(cpu, (ushort) ((opcode & 0x0F00) >> 2), (ushort) ((opcode & 0x00FF))); } break;
-      case 0x8000: switch (opcode & 0x000F)
-                   {
-                     case 0x0000: { LD_VxVy_8XY0(cpu, (ushort) ((opcode & 0x0F00) >> 2), (ushort) ((opcode & 0x00F0) >> 1)); } break;
-                     case 0x0001: { OR_VxVy_8XY1(cpu, (ushort) ((opcode & 0x0F00) >> 2), (ushort) ((opcode & 0x00F0) >> 1)); } break;
-                     case 0x0002: { AND_VxVy_8XY2(cpu, (ushort) ((opcode & 0x0F00) >> 2), (ushort) ((opcode & 0x00F0) >> 1)); } break;
-                     case 0x0003: { XOR_VxVy_8XY3(cpu, (ushort) ((opcode & 0x0F00) >> 2), (ushort) ((opcode & 0x00F0) >> 1)); } break;
-                     case 0x0004: { ADD_VxVy_8XY4(cpu, (ushort) ((opcode & 0x0F00) >> 2), (ushort) ((opcode & 0x00F0) >> 1)); } break;
-                     case 0x0005: { SUB_VxVy_8XY5(cpu, (ushort) ((opcode & 0x0F00) >> 2), (ushort) ((opcode & 0x00F0) >> 1)); } break;
-                     case 0x0006: { SHR_Vx_Vy_8XY6(cpu, (ushort) ((opcode & 0x0F00) >> 2), (ushort) ((opcode & 0x00F0) >> 1)); } break;
-                     case 0x0007: { SUBN_VxVy_8XY7(cpu, (ushort) ((opcode & 0x0F00) >> 2), (ushort) ((opcode & 0x00F0) >> 1)); } break;
-                     case 0x000E: { SHL_Vx_Vy_8XYE(cpu, (ushort) ((opcode & 0x0F00) >> 2), (ushort) ((opcode & 0x00F0) >> 1)); } break;
-                   } break;
+      case 0x1000: { JP_ADR_1NNN(cpu, (ushort)(opcode & 0x0FFF)); } break;
+      case 0x2000: { CALL_ADR_2NNN(cpu, (ushort)(opcode & 0x0FFF)); } break;
+      case 0x3000: { SE_Vx_3XKK(cpu, (ushort)((opcode & 0x0F00) >> 2), (ushort)(opcode & 0x00FF)); } break;
+      case 0x4000: { SNE_Vx_4XKK(cpu, (ushort)((opcode & 0x0F00) >> 2), (ushort)(opcode & 0x00FF)); } break;
+      case 0x5000: { SE_VxVy_5XY0(cpu, (ushort)((opcode & 0x0F00) >> 2), (ushort)((opcode & 0x00F0) >> 1)); } break;
+      case 0x6000: { LD_Vx_6XKK(cpu, (ushort)((opcode & 0x0F00) >> 2), (ushort)((opcode & 0x00FF) >> 1)); } break;
+      case 0x7000: { ADD_Vx_7XKK(cpu, (ushort)((opcode & 0x0F00) >> 2), (ushort)((opcode & 0x00FF))); } break;
+      case 0x8000:
+        switch (opcode & 0x000F)
+        {
+          case 0x0000: { LD_VxVy_8XY0(cpu, (ushort)((opcode & 0x0F00) >> 2), (ushort)((opcode & 0x00F0) >> 1)); } break;
+          case 0x0001: { OR_VxVy_8XY1(cpu, (ushort)((opcode & 0x0F00) >> 2), (ushort)((opcode & 0x00F0) >> 1)); } break;
+          case 0x0002: { AND_VxVy_8XY2(cpu, (ushort)((opcode & 0x0F00) >> 2), (ushort)((opcode & 0x00F0) >> 1)); } break;
+          case 0x0003: { XOR_VxVy_8XY3(cpu, (ushort)((opcode & 0x0F00) >> 2), (ushort)((opcode & 0x00F0) >> 1)); } break;
+          case 0x0004: { ADD_VxVy_8XY4(cpu, (ushort)((opcode & 0x0F00) >> 2), (ushort)((opcode & 0x00F0) >> 1)); } break;
+          case 0x0005: { SUB_VxVy_8XY5(cpu, (ushort)((opcode & 0x0F00) >> 2), (ushort)((opcode & 0x00F0) >> 1)); } break;
+          case 0x0006: { SHR_Vx_Vy_8XY6(cpu, (ushort)((opcode & 0x0F00) >> 2), (ushort)((opcode & 0x00F0) >> 1)); } break;
+          case 0x0007: { SUBN_VxVy_8XY7(cpu, (ushort)((opcode & 0x0F00) >> 2), (ushort)((opcode & 0x00F0) >> 1)); } break;
+          case 0x000E: { SHL_Vx_Vy_8XYE(cpu, (ushort)((opcode & 0x0F00) >> 2), (ushort)((opcode & 0x00F0) >> 1)); } break;
+        }
+        break;
       case 0x9000: break;
 
-      case 0xA000: { LD_I_ADR_ANNN(cpu, (ushort) (opcode & 0x0FFF)); } break;
-      case 0xB000: { JP_V0_ADR_BNNN(cpu, (ushort) (opcode & 0x0FFF)); } break;
-      case 0xC000: { RND_Vx_BYTE_CXKK(cpu, (ushort) ((opcode & 0x0F00) >> 2), (ushort) (opcode & 0x00FF)); }break;
+      case 0xA000: { LD_I_ADR_ANNN(cpu, (ushort)(opcode & 0x0FFF)); } break;
+      case 0xB000: { JP_V0_ADR_BNNN(cpu, (ushort)(opcode & 0x0FFF)); } break;
+      case 0xC000: { RND_Vx_BYTE_CXKK(cpu, (ushort)((opcode & 0x0F00) >> 2), (ushort)(opcode & 0x00FF)); } break;
       case 0xD000: break;
       case 0xE000: break;
       case 0xF000: break;
@@ -56,7 +66,7 @@ class CPU()
     }
   }
 
-#region OPCODES
+  #region OPCODES
 
   /// <summary>
   ///   Scroll the screen down x lines	Super only, not implemented
@@ -105,7 +115,7 @@ class CPU()
   /// <param name="kk"></param>
   /// <exception cref="NotImplementedException"></exception>
   private void SE_Vx_3XKK(CPU cpu, ushort x, ushort kk) => throw new NotImplementedException();
-  
+
   /// <summary>
   ///   skip if register r <> constant
   /// </summary>
@@ -114,7 +124,7 @@ class CPU()
   /// <param name="kk"></param>
   /// <exception cref="NotImplementedException"></exception>
   private void SNE_Vx_4XKK(CPU cpu, ushort x, ushort kk) => throw new NotImplementedException();
-  
+
   /// <summary>
   ///   skip f register r = register y
   /// </summary>
@@ -123,7 +133,7 @@ class CPU()
   /// <param name="y"></param>
   /// <exception cref="NotImplementedException"></exception>
   private void SE_VxVy_5XY0(CPU cpu, ushort x, ushort y) => throw new NotImplementedException();
-  
+
   /// <summary>
   ///   move constant to register r
   /// </summary>
@@ -132,7 +142,7 @@ class CPU()
   /// <param name="kk"></param>
   /// <exception cref="NotImplementedException"></exception>
   private void LD_Vx_6XKK(CPU cpu, ushort x, ushort kk) => throw new NotImplementedException();
-  
+
   /// <summary>
   ///   add constant to register r	No carry generated
   /// </summary>
@@ -141,7 +151,7 @@ class CPU()
   /// <param name="kk"></param>
   /// <exception cref="NotImplementedException"></exception>
   private void ADD_Vx_7XKK(CPU cpu, ushort x, ushort kk) => throw new NotImplementedException();
-  
+
   /// <summary>
   ///   move register vy into vx
   /// </summary>
@@ -150,7 +160,7 @@ class CPU()
   /// <param name="y"></param>
   /// <exception cref="NotImplementedException"></exception>
   private void LD_VxVy_8XY0(CPU cpu, ushort x, ushort y) => throw new NotImplementedException();
-  
+
   /// <summary>
   ///   or register vy into register vx
   /// </summary>
@@ -159,7 +169,7 @@ class CPU()
   /// <param name="y"></param>
   /// <exception cref="NotImplementedException"></exception>
   private void OR_VxVy_8XY1(CPU cpu, ushort x, ushort y) => throw new NotImplementedException();
-  
+
   /// <summary>
   /// 	and register vy into register vx
   /// </summary>
@@ -168,7 +178,7 @@ class CPU()
   /// <param name="y"></param>
   /// <exception cref="NotImplementedException"></exception>
   private void AND_VxVy_8XY2(CPU cpu, ushort x, ushort y) => throw new NotImplementedException();
-  
+
   /// <summary>
   ///   exclusive or register ry into register rx
   /// </summary>
@@ -177,7 +187,7 @@ class CPU()
   /// <param name="y"></param>
   /// <exception cref="NotImplementedException"></exception>
   private void XOR_VxVy_8XY3(CPU cpu, ushort x, ushort y) => throw new NotImplementedException();
-  
+
   /// <summary>
   /// add register vy to vx,carry in vf
   /// </summary>
@@ -186,7 +196,7 @@ class CPU()
   /// <param name="y"></param>
   /// <exception cref="NotImplementedException"></exception>
   private void ADD_VxVy_8XY4(CPU cpu, ushort x, ushort y) => throw new NotImplementedException();
-  
+
   /// <summary>
   ///   	subtract register vy from vr,borrow in vf	vf set to 1 if borroesws
   /// </summary>
@@ -195,7 +205,7 @@ class CPU()
   /// <param name="y"></param>
   /// <exception cref="NotImplementedException"></exception>
   private void SUB_VxVy_8XY5(CPU cpu, ushort x, ushort y) => throw new NotImplementedException();
-  
+
   /// <summary>
   ///   shift register vy right, bit 0 goes into register vf
   /// </summary>
@@ -204,7 +214,7 @@ class CPU()
   /// <param name="y"></param>
   /// <exception cref="NotImplementedException"></exception>
   private void SHR_Vx_Vy_8XY6(CPU cpu, ushort x, ushort y) => throw new NotImplementedException();
-  
+
   /// <summary>
   ///   	subtract register vr from register vy, result in vr	vf set to 1 if borrows
   /// </summary>
@@ -213,7 +223,7 @@ class CPU()
   /// <param name="y"></param>
   /// <exception cref="NotImplementedException"></exception>
   private void SUBN_VxVy_8XY7(CPU cpu, ushort x, ushort y) => throw new NotImplementedException();
-  
+
   /// <summary>
   /// 	shift register vr left,bit 7 goes into register vf
   /// </summary>
@@ -222,7 +232,7 @@ class CPU()
   /// <param name="y"></param>
   /// <exception cref="NotImplementedException"></exception>
   private void SHL_Vx_Vy_8XYE(CPU cpu, ushort x, ushort y) => throw new NotImplementedException();
-  
+
   /// <summary>
   /// 	skip if register rx <> register ry
   /// </summary>
@@ -231,7 +241,7 @@ class CPU()
   /// <param name="y"></param>
   /// <exception cref="NotImplementedException"></exception>
   private void SND_VxVy_9XY0(CPU cpu, ushort x, ushort y) => throw new NotImplementedException();
-  
+
   /// <summary>
   /// 	Load index register with constant nnn
   /// </summary>
@@ -239,7 +249,7 @@ class CPU()
   /// <param name="nnn"></param>
   /// <exception cref="NotImplementedException"></exception>
   private void LD_I_ADR_ANNN(CPU cpu, ushort nnn) => throw new NotImplementedException();
-  
+
   /// <summary>
   ///   Jump to address nnn+register v0
   /// </summary>
@@ -247,7 +257,7 @@ class CPU()
   /// <param name="nnn"></param>
   /// <exception cref="NotImplementedException"></exception>
   private void JP_V0_ADR_BNNN(CPU cpu, ushort nnn) => throw new NotImplementedException();
-  
+
   /// <summary>
   /// vx = random number less than or equal to kkk
   /// </summary>
@@ -256,7 +266,7 @@ class CPU()
   /// <param name="kk"></param>
   /// <exception cref="NotImplementedException"></exception>
   private void RND_Vx_BYTE_CXKK(CPU cpu, ushort x, ushort kk) => throw new NotImplementedException();
-  
+
   /// <summary>
   ///   Draw sprite at screen location rx,ry height s	
   ///     Sprites stored in memory at location in index register, 
@@ -279,7 +289,7 @@ class CPU()
   /// <param name="x"></param>
   /// <exception cref="NotImplementedException"></exception>
   private void SKP_Vx_EX9E(CPU cpu, ushort x) => throw new NotImplementedException();
-  
+
   /// <summary>
   ///   skip if key (register rk) not pressed
   /// </summary>
@@ -287,7 +297,7 @@ class CPU()
   /// <param name="x"></param>
   /// <exception cref="NotImplementedException"></exception>
   private void SKNP_Vx_EXA1(CPU cpu, ushort x) => throw new NotImplementedException();
-  
+
   /// <summary>
   ///   get delay timer into vr
   /// </summary>
@@ -295,7 +305,7 @@ class CPU()
   /// <param name="x"></param>
   /// <exception cref="NotImplementedException"></exception>
   private void LD_Vx_dt_FX07(CPU cpu, ushort x) => throw new NotImplementedException();
-  
+
   /// <summary>
   ///   wait for for keypress,put key in register vr
   /// </summary>
@@ -303,7 +313,7 @@ class CPU()
   /// <param name="x"></param>
   /// <exception cref="NotImplementedException"></exception>
   private void LD_VxK_FX0A(CPU cpu, ushort x) => throw new NotImplementedException();
-  
+
   /// <summary>
   ///   set the delay timer to vr
   /// </summary>
@@ -311,7 +321,7 @@ class CPU()
   /// <param name="x"></param>
   /// <exception cref="NotImplementedException"></exception>
   private void LD_dt_Vx_FX15(CPU cpu, ushort x) => throw new NotImplementedException();
-  
+
   /// <summary>
   ///   set the sound timer to vr
   /// </summary>
@@ -319,7 +329,7 @@ class CPU()
   /// <param name="x"></param>
   /// <exception cref="NotImplementedException"></exception>
   private void LD_st_Vx_FX18(CPU cpu, ushort x) => throw new NotImplementedException();
-  
+
   /// <summary>
   ///   add register vr to the index register
   /// </summary>
@@ -327,7 +337,7 @@ class CPU()
   /// <param name="x"></param>
   /// <exception cref="NotImplementedException"></exception>
   private void ADD_IVx_FX1E(CPU cpu, ushort x) => throw new NotImplementedException();
-  
+
   /// <summary>
   ///   point I to the sprite for hexadecimal character in vr	Sprite is 5 bytes high
   /// </summary>
@@ -335,7 +345,7 @@ class CPU()
   /// <param name="x"></param>
   /// <exception cref="NotImplementedException"></exception>
   private void LD_FVx_FX29(CPU cpu, ushort x) => throw new NotImplementedException();
-  
+
   /// <summary>
   ///   store the bcd representation of register vr at location I,I+1,I+2	Doesn't change I
   /// </summary>
@@ -343,7 +353,7 @@ class CPU()
   /// <param name="x"></param>
   /// <exception cref="NotImplementedException"></exception>
   private void LD_BVx_FX33(CPU cpu, ushort x) => throw new NotImplementedException();
-  
+
   /// <summary>
   ///   store registers v0-vr at location I onwards	I is incremented 
   ///     to point to the next location on. 
@@ -354,7 +364,7 @@ class CPU()
   /// <param name="x"></param>
   /// <exception cref="NotImplementedException"></exception>
   private void LD_ADRIVx_FX55(CPU cpu, ushort x) => throw new NotImplementedException();
-  
+
   /// <summary>
   ///   load registers v0-vr from location I onwards	as above.
   /// </summary>
--- Chip8App/Models/Display.cs
+++ Chip8App/Models/Display.cs
@@ -11,8 +11,6 @@ class Display
   public const int DISPLAY_HEIGHT = 32;
   Texture2D texture2D = new();
   private readonly float scale = 1;
-  public Color[] PixelBuffer { get; } =
-      new Color[DISPLAY_WIDTH * DISPLAY_HEIGHT];
 
   public Display(int width, int height, string title, float scale)
   {
@@ -26,37 +24,52 @@ class Display
     this.scale = scale;
   }
 
-  public bool DrawSinglePixel(int x, int y, Color color)
+  public static bool DrawSinglePixel(int x, int y, bool on, ref bool[] display_buffer)
   {
     var idx = (y * DISPLAY_WIDTH) + x;
 
-    if (idx < 0 || idx >= PixelBuffer.Length) return false;
+    if (idx < 0 || idx >= display_buffer.Length) return false;
 
-    PixelBuffer[idx] = color;
+    display_buffer[idx] = on;
     return true;
   }
 
-  public void Draw()
+  public static bool NumIntoBuffer(Vector2 start, ushort data, ref bool[] display_buffer)
+  {
+    var x = (int)start.X;
+    var y = (int)start.Y;
+
+    for (int i = 0; i < 16; i++)
+    {
+      int bitIdx = 15 - i;
+      bool bit = ((data >> bitIdx) & 1) == 1;
+
+      int bufferIdx = (y * DISPLAY_WIDTH) + x + i;
+
+      if (bufferIdx < 0 || bufferIdx >= display_buffer.Length) return false;
+
+      display_buffer[bufferIdx] = bit;
+    }
+
+    return true;
+  }
+  public void Draw(ref bool[] display_buffer)
   {
     Raylib.BeginDrawing();
     Raylib.ClearBackground(Color.RayWhite);
 
-    for (int i = 0; i < PixelBuffer.Length; i++)
+    var color_buffer = new Color[display_buffer.Length];
+
+    for (int i = 0; 0 < display_buffer.Length; i++)
     {
-      PixelBuffer[i] = (i % 3) switch
-      {
-        0 => Color.Green,
-        1 => Color.Red,
-        2 => Color.Blue,
-        _ => Color.Black,
-      };
+      color_buffer[i] = display_buffer[i] ? Color.Black : Color.White;
     }
 
     var screenWidth = Raylib.GetScreenWidth();
     var chipWidth = 64 * scale;
     var x_offset = (screenWidth - chipWidth) / 2;
 
-    UpdateTextureUnsafe(texture2D, PixelBuffer);
+    UpdateTextureUnsafe(texture2D, color_buffer);
 
     Raylib.DrawTextureEx(texture2D,
         new Vector2(x_offset, 0),
@@ -85,7 +98,6 @@ class Display
     0x90,
     0xF0,
   ];
-
   public static readonly ushort[] ONE = [
     0x20,
     0x60,
@@ -100,65 +112,95 @@ class Display
     0x80,
     0xF0
   ];
-
   public static readonly ushort[] THREE = [
-    0xF0,
-    0x90,
-    0x90,
-    0x90,
-    0xF0,
-  ];
-
-  public static readonly ushort[] FOUR = [
-    0x20,
-    0x60,
-    0x20,
-    0x20,
-    0x70
-  ];
-  public static readonly ushort[] FIVE = [
     0xF0,
     0x10,
     0xF0,
+    0x10,
+    0xF0,
+  ];
+  public static readonly ushort[] FOUR = [
+    0x90,
+    0x90,
+    0xF0,
+    0x10,
+    0x10
+  ];
+  public static readonly ushort[] FIVE = [
+    0xF0,
     0x80,
+    0xF0,
+    0x10,
     0xF0
   ];
-
   public static readonly ushort[] SIX = [
-    0x20,
-    0x60,
-    0x20,
-    0x20,
-    0x70
+    0xF0,
+    0x80,
+    0xF0,
+    0x90,
+    0xF0
   ];
-  public static readonly ushort[] Seven= [
+  public static readonly ushort[] Seven = [
     0xF0,
     0x10,
+    0x20,
+    0x40,
+    0x40
+  ];
+  public static readonly ushort[] EIGHT = [
+    0xF0,
+    0x90,
+    0xF0,
+    0x90,
+    0xF0,
+  ];
+  public static readonly ushort[] NINE = [
+    0xF0,
+    0x90,
+    0xF0,
+    0x10,
+    0xF0
+  ];
+  public static readonly ushort[] A = [
+    0xF0,
+    0x90,
+    0xF0,
+    0x90,
+    0xF0
+  ];
+  public static readonly ushort[] B = [
+    0xE0,
+    0x90,
+    0xE0,
+    0x90,
+    0xE0
+  ];
+  public static readonly ushort[] C = [
+    0xF0,
+    0x80,
+    0x80,
+    0x80,
+    0xF0
+  ];
+  public static readonly ushort[] D = [
+    0xE0,
+    0x90,
+    0x90,
+    0x90,
+    0xE0,
+  ];
+  public static readonly ushort[] E = [
+    0xF0,
+    0x80,
     0xF0,
     0x80,
     0xF0
   ];
-
-  public static readonly ushort[] THREE = [
-    0xF0,
-    0x90,
-    0x90,
-    0x90,
-    0xF0,
-  ];
-
-  public static readonly ushort[] FOUR = [
-    0x20,
-    0x60,
-    0x20,
-    0x20,
-    0x70
-  ];
-  public static readonly ushort[] FIVE = [
-    0xF0,
-    0x10,
+  public static readonly ushort[] F = [
     0xF0,
     0x80,
-    0xF0
+    0xF0,
+    0x80,
+    0x80
   ];
 }
